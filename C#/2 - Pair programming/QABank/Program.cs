11375d1eead1d734dfbf505ae60866b45431404d
Alex Comerford <Alex.Comerford@atos.net>
2022-10-10 14:42:35 +0100

    Lab 2 task1

--- C#/2 - Pair programming/QABank/Program.cs
+++ C#/2 - Pair programming/QABank/Program.cs
@@ -10,6 +10,51 @@ namespace QABank
     {
         static void Main(string[] args)
         {
+            BankAccount b1 = new BankAccount("Alex Comerford",10, 100.50);
+
+            BankAccount b2 = new BankAccount("James Smith", 500.50);
+
+            BankAccount b3 = new BankAccount("Holly Hope", 2500);
+            b1.Display();
+            b2.Display();
+            b3.Display();
+            Console.ReadLine();
         }
     }
+
+    class BankAccount
+    {
+        string Customer;
+        public int AccountNumber = 100000;
+        double Balance;
+
+        public BankAccount(string customer, int accountnumber, double balance)
+        {
+            Customer = customer;
+            AccountNumber = accountnumber;
+            Balance = balance;
+        }
+        public BankAccount(string customer, double balance)
+        {
+            Customer = customer;
+            AccountNumber++;
+            Balance = balance;
+        }
+
+        public int Deposit(int amount)
+        {
+            return (int)(Balance + amount);
+        }
+
+        public int Withdraw(int amount)
+        {
+            return (int)(Balance - amount);
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Name: " + Customer + " Account Number: " + AccountNumber + " Balance: " + Balance);
+        }
+    }
+
 }
