36d63f4390fed2350c56d04a667456b01df3db60
Alex Comerford <Alex.Comerford@atos.net>
2022-10-11 16:02:11 +0100

    lab 4.4

---
+++ C#/4.3 - Test coverage/QACalculator/Calculate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QACalculator
+{
+    public class Calculate
+    {
+        public bool isEven(int num)
+        {
+            if (num % 2 == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ C#/4.3 - Test coverage/QACalculatorTests/CalculateTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QACalculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QACalculator.Tests
+{
+    [TestClass()]
+    public class CalculateTests
+    {
+        [DataTestMethod]
+        [DataRow(1, false)]
+        [DataRow(2, true)]
+        [DataRow(4, true)]
+        [DataRow(500, true)]
+        public void isEvenTest(int n, bool b)
+        {
+            Calculate c = new Calculate();
+            bool result = c.isEven(n);
+            Assert.AreEqual(b, result);
+        }
+
+        [TestMethod]
+        public void TestEvenManual()
+        {
+            Calculate c = new Calculate();
+            Assert.AreEqual(true, c.isEven(20));
+            
+        }
+    }
+}
+
+
